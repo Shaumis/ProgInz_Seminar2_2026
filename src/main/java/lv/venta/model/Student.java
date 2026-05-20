d3bbf61acb8214d384385e6f9700df582267c987
s25abelmiku <michisillproductions@gmail.com>
2026-05-20 16:08:24 +0300

    GETSET

---
+++ src/main/java/lv/venta/model/Student.java
@@ -0,0 +1,23 @@
+package lv.venta.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Student {
+	@Setter(value = AccessLevel.NONE)
+	private long ids;
+	
+	private String name;
+	private String surname;
+	
+	public Student(String name, String surname) {
+		
+	}
+}
