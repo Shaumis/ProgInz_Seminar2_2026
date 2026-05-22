f9788dce784098b7e9a05501c18a836e670a26a7
s25abelmiku <michisillproductions@gmail.com>
2026-05-22 18:20:38 +0300

    Made Example Data

--- src/main/java/lv/venta/ProgInzSeminar2Application.java
+++ src/main/java/lv/venta/ProgInzSeminar2Application.java
@@ -1,7 +1,20 @@
 package lv.venta;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lv.venta.model.Course;
+import lv.venta.model.Grade;
+import lv.venta.model.Professor;
+import lv.venta.model.Student;
+import lv.venta.model.enumarator.Degree;
+import lv.venta.repo.ICourseRepo;
+import lv.venta.repo.IGradeRepo;
+import lv.venta.repo.IProfessorRepo;
+import lv.venta.repo.IStudentRepo;
 
 @SpringBootApplication
 public class ProgInzSeminar2Application {
@@ -9,5 +22,27 @@ public class ProgInzSeminar2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ProgInzSeminar2Application.class, args);
 	}
-
+	public CommandLineRunner saveDataInDB(IStudentRepo studRepo, IProfessorRepo profRepo, IGradeRepo gradRepo, ICourseRepo courRepo) {
+		
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				Professor p1 = new Professor("Karina", "Šķirmante", Degree.Master);
+				Professor p2 = new Professor("Karlis", "immers", Degree.Master);
+				profRepo.saveAll(Arrays.asList(p1,p2));
+				Student s1 = new Student("Mikus", "Ābele");
+				Student s2 = new Student("Ivo", "Vilnis");
+				studRepo.saveAll(Arrays.asList(s1,s2));
+				Course c1 = new Course("Programēšana JAVA", 4, p1);
+				Course c2 = new Course("Web tehnoloģijas", 3, p2);
+				courRepo.saveAll(Arrays.asList(c1,c2));
+				Grade g1 = new Grade(10, s1, c1);
+				Grade g2 = new Grade(9, s2, c2);
+				gradRepo.saveAll(Arrays.asList(g1,g2));
+			}
+		};
+	}
+	
 }
+
