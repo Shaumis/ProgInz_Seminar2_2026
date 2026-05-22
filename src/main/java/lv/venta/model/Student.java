d6c44efe8bbcc57a25779e9ed71fabeab5236dcd
s25abelmiku <michisillproductions@gmail.com>
2026-05-22 16:48:19 +0300

    Made Student class

--- src/main/java/lv/venta/model/Student.java
+++ src/main/java/lv/venta/model/Student.java
@@ -1,5 +1,15 @@
 package lv.venta.model;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,11 +20,24 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
+@Table(name = "StudentTable")
+@Entity
 public class Student {
+	@Column(name="Ids")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Setter(value = AccessLevel.NONE)
 	private long ids;
-
+	
+	@NotEmpty
+	@NotNull
+	@Pattern(regexp = "[A-Ž]{1}[a-ž]{2,20}([ ]{1}([a-ž]{2,20}))?")
+	@Column(name = "Name")
 	private String name;
+	@NotEmpty
+	@NotNull
+	@Pattern(regexp = "[A-Ž]{1}[a-ž]{2,20}([ ]{1}([a-ž]{2,20}))?")
+	@Column(name = "Surname")
 	private String surname;
 
 	public Student(String name, String surname) {
