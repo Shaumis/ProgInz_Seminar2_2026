e48651626647cec070227ce3b11222dcd526e86c
s25abelmiku <michisillproductions@gmail.com>
2026-05-22 17:13:25 +0300

    Made Course class

---
+++ src/main/java/lv/venta/model/Course.java
@@ -0,0 +1,50 @@
+package lv.venta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "CourseTable")
+@Entity
+public class Course {
+	@Column(name = "Idc", unique = true)
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idc;
+
+	@NotEmpty
+	@NotNull
+	@Pattern(regexp = "[A-Ž]{1}[A-Ža-ž0-9 ]{2,40}")
+	@Column(name = "Title", unique = true)
+	private String title;
+	
+	@Min(1)
+	@Max(40)
+	@Column(name = "CP")
+	private int credit;
+
+	public Course(String title, int credit) {
+		setTitle(title);
+		setCredit(credit);
+
+	}
+
+}
