93c789f745036a8985a687562bf9c15643c6e040
s25abelmiku <michisillproductions@gmail.com>
2026-05-29 17:33:34 +0300

    Crud Student

--- src/main/java/lv/venta/service/ICRUDBaseService.java
+++ src/main/java/lv/venta/service/ICRUDBaseService.java
@@ -3,7 +3,11 @@ package lv.venta.service;
 import java.util.ArrayList;
 
 public interface ICRUDBaseService<Ttype> {
-	public abstract ArrayList<Ttype> retrieveAll()throws Exception;
+	public abstract void createNew(Ttype newObject)throws Exception;
+	
+	public abstract ArrayList<Ttype> retrieveAll() throws Exception;
+
 	public abstract Ttype retrieveById(long id) throws Exception;
+
 	public abstract void deleteById(long id) throws Exception;
 }
